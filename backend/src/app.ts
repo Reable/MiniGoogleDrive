515d196ac90b4a913d0ac95b60a68afd382c9afa
Razmyslov <razmyslov.ivan.2004@gmail.com>
2022-07-27 02:35:34 +0300

    download eslint, prettier, typescript for backend, and download react with typescript

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import express, { Express, Request, Response } from 'express';
+
+const app: Express = express();
+
+app.listen(3001, () => {
+  console.log('Server running http://localhost:3001');
+});
+
+app.get('/api', (_req: Request, res: Response) => {
+  res.json({
+    message: 'TS ok!',
+  });
+});
