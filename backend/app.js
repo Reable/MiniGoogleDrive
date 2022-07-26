61d1b677b36189ad737da184638fe8d35696f68f
Razmyslov <razmyslov.ivan.2004@gmail.com>
2022-07-26 16:40:33 +0300

    last test upd

--- backend/app.js
+++ backend/app.js
@@ -6,12 +6,6 @@ app.listen(3001,()=>{
   console.log('Server is running port 3001')
 })
 
-app.get('/api/test',(req,res)=>{
-  res.json({
-    message: 'test ! '
-  })
-})
-
 app.get('/api',(req,res)=>{
   res.json({
     message:' Hellow tadadaddadasest)'
