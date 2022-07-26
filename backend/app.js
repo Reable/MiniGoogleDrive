c0e07900492bdef0eecd50606eec6929ce8516c1
Иван Размыслов <razmyslov@MacBook-Air-Ivan.local>
2022-07-26 15:02:44 +0300

    test

--- backend/app.js
+++ backend/app.js
@@ -6,8 +6,15 @@ app.listen(3001,()=>{
   console.log('Server is running port 3001')
 })
 
+app.get('/api/test',(req,res)=>{
+  res.json({
+    message: 'test'
+  })
+})
+
 app.get('/api',(req,res)=>{
   res.json({
     message:' Hellow tadadaddadasest)'
   })
 })
+
