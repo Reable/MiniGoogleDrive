e42bcaff3e3ca59d2ab8e3b7a35a53d2e20d8477
Razmyslov <razmyslov.ivan.2004@gmail.com>
2022-07-27 11:59:41 +0300

    eslint plugins

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,13 @@
-import express, { Express, Request, Response } from 'express';
+import express, { Express, Request, Response } from "express";
 
 const app: Express = express();
 
 app.listen(3001, () => {
-  console.log('Server running http://localhost:3001');
+  console.log("Server running http://localhost:3001");
 });
 
-app.get('/api', (_req: Request, res: Response) => {
+app.get("/api", (_req: Request, res: Response) => {
   res.json({
-    message: 'TS ok!',
+    message: "TS ok!",
   });
 });
