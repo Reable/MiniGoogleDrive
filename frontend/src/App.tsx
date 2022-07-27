9c1b771ebabe712c023b7e7f07911c73de7013b3
Razmyslov <razmyslov.ivan.2004@gmail.com>
2022-07-27 13:05:42 +0300

    upd eslint

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,15 +1,15 @@
-import React from 'react'
+import React from "react";
 
 function App() {
-  const hello = ''
-  console.log(hello)
+  const hello = "";
+  console.log(hello);
   return (
     <div className="App">
       <header className="App-header">
         Lorem, ipsum.
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
