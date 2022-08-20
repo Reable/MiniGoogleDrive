c3b3e59fcea2307878cdeb9ff1b6c56c33218944
Reabletop <razmyslov.ivan.2004@gmail.com>
2022-08-20 21:59:37 +0300

    upd

--- src/db.js
+++ src/db.js
@@ -1,6 +1,6 @@
 const Sequelize = require('sequelize');
 
-const sequelize = new Sequelize("googleDrive", "mysql", "mysql", { 
+const sequelize = new Sequelize("googleDrive", "root", "root", { 
   host: "localhost", 
   dialect: "mysql",
 })
